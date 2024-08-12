8bd4440358d19b732b13c71219d432e0fbf10cbb
Jakub Senko <m@jsenko.net>
2024-08-12 21:34:23 +0200

    w

--- operator/operator/src/test/java/io/apicurio/registry/operator/test/SmokeIT.java
+++ operator/operator/src/test/java/io/apicurio/registry/operator/test/SmokeIT.java
@@ -17,7 +17,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.time.Duration;
 
 import static io.apicurio.registry.operator.test.OperatorTestExtension.LONG_WAIT;
 import static java.time.Duration.ofSeconds;
